f6e1b8bf78d8a7bd32eb7663afeeec35849c2fcb
George Zografos <george.p.zografos@gmail.com>
2014-11-16 19:22:28 +0200

    Refactor

--- account/account-weekly-performance-report.js
+++ account/account-weekly-performance-report.js
@@ -3,13 +3,13 @@ Copyright (c) 2013, Growth
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met: 
+modification, are permitted provided that the following conditions are met:
 
 1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer. 
+   list of conditions and the following disclaimer.
 2. Redistributions in binary form must reproduce the above copyright notice,
    this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution. 
+   and/or other materials provided with the distribution.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -23,56 +23,83 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-// Outputs the performance of an account during the previous week (Sun-Mon) 
-// to a google spreadsheet.
-// If scheduled, every week the program appends the new performance data to
-// the specified spreadsheet.
-var REPORT_URL = '*******';
+/**
+ *
+ * @author George Zografos
+ * @name Account weekly performance report
+ * @description  Outputs the performance of an account during the previous week (Sun-Mon)
+ *               to a google spreadsheet. If scheduled, every week the program
+ *               appends the new performance data to the specified spreadsheet.
+ */
+
+/**
+ * The url of the spreadsheet to export the data to
+ */
+var SPREADSHEET_URL = 'http://...';
+
+/**
+ * The name of the sheet in which to add the data in.
+ * If no name is supplied then then the first sheet of the spreadsheet will be used
+ */
+var SHEET_NAME = '';
+
+/**
+ * Used when formatting the current date. For more info check the following url:
+ * https://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html
+ */
+var TIMEZONE = '+3';
+var DATE_FORMAT = 'E, MMM d, YYYY';
+
+/**
+ * The account performance fields that will be exported to the spreadsheet
+ */
+var PERFORMANCE_FIELDS = [
+    'Impressions',
+    'Clicks',
+    'Cost',
+    'Conversions',
+    'CostPerConversion',
+    'ConversionRate',
+    'ViewThroughConversions'
+];
 
 function main() {
-    var spreadsheet = SpreadsheetApp.openByUrl(REPORT_URL);
+    var performanceResults = getAccountPerformanceResults();
 
-    var sheet = spreadsheet.getActiveSheet();
+    performanceResults.unshift( getFormattedDate() );
 
-    var fields = [
-        "Impressions",
-        "Clicks",
-        "Cost",
-        "Conversions",
-        "CostPerConversion",
-        "ConversionRate",
-        "ViewThroughConversions"
+    exportToSpreadsheet( performanceResults );
+}
+
+function getAccountPerformanceResults() {
+
+    var awql = 'SELECT ' + PERFORMANCE_FIELDS.join( ',' ) +
+        ' FROM ACCOUNT_PERFORMANCE_REPORT ' +
+        ' DURING LAST_WEEK';
+
+    var report = AdWordsApp.report( awql );
+
+    var row = report.rows().next();
+
+    return [
+        row.Impressions,
+        row.Clicks,
+        row.Cost,
+        row.Conversions,
+        row.CostPerConversion,
+        row.ConversionRate,
+        row.ViewThroughConversions
     ];
+}
 
-    var awql = "SELECT " + fields.join(",") +
-        " FROM ACCOUNT_PERFORMANCE_REPORT " +
-        " DURING LAST_WEEK";
+// For example: Week of Sat, Nov 15, 2014
+function getFormattedDate() {
+    var formattedDate = Utilities.formatDate( new Date(), TIMEZONE, DATE_FORMAT )
+    return Utilities.formatString( 'Week of %s', formattedDate );
+}
 
-    var report = AdWordsApp.report(awql);
-
-    var rows = report.rows();
-    var row = rows.next();
-    // Format date
-    var date = new Date();
-    var formattedDate = Utilities.formatDate(new Date(), "GMT+3", "E");
-    var dateString = date.toDateString();
-    var day = dateString.substr(0, 3);
-    var mon = dateString.substr(4, 3);
-    date = "Week of " + formattedDate + ", " + mon + " " + date.getDate() + ", " + date.getYear();
-
-    // Export result
-    var result = [
-        [date,
-            row.Impressions,
-            row.Clicks,
-            row.Cost,
-            row.Conversions,
-            row.CostPerConversion,
-            row.ConversionRate,
-            row.ViewThroughConversions
-        ]
-    ];
-
-    var range = sheet.getRange(sheet.getLastRow() + 1, 1, 1, result[0].length);
-    range.setValues(result);
+function exportToSpreadsheet( performanceResults ) {
+    var spreadsheet = SpreadsheetApp.openByUrl( SPREADSHEET_URL );
+    var sheet = SHEET_NAME ? spreadsheet.getSheetByName( SHEET_NAME ) : spreadsheet.getSheets()[ 0 ];
+    sheet.appendRow( performanceResults );
 }
